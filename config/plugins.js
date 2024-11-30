ee4f4555cb43e24c8d3e8532955887be739b51bd
alxworld <jsalex@gmail.com>
2024-11-30 23:06:21 +0530

    s3Options added in plugins

--- config/plugins.js
+++ config/plugins.js
@@ -6,19 +6,24 @@ module.exports = ({ env }) => ({
       jwtSecret: env('ADMIN_JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
     },
   },
+  // ...
   upload: {
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        credentials: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-        },
-        region: env('AWS_REGION'),
-        params: {
-          // ACL: 'private', // <== set ACL to private
-          // signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-          Bucket: env('AWS_BUCKET'),
+        // baseUrl: env('CDN_URL'),
+        // rootPath: env('CDN_ROOT_PATH'),
+        s3Options: {
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+          },
+          region: env('AWS_REGION'),
+          params: {
+            // ACL: env('AWS_ACL', 'public-read'),
+            // signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('AWS_BUCKET'),
+          },
         },
       },
       actionOptions: {
@@ -28,4 +33,5 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  // ...
 })
